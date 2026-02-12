1ca2908e8e4b90d644cdf9afbd76c4bc07be6d55
Volodymyr Pestov <pestov.v@yahoo.com>
2026-02-13 01:45:03 +0200

    changed cv

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -71,7 +71,7 @@ export const Header: NextPage = () => {
 
         <div className="animate-on-scroll fade-in-up stagger-3">
           <a
-            href="/Volodymyr_Pestov_CV.pdf"
+            href="/Volodymyr_Pestov.pdf"
             target="_blank"
             rel="noreferrer"
             className={style.download}
