a4c9eee92f6f4c6ff51ac93e519e152290ef9f05
Volodymyr Pestov <pestov.v@yahoo.com>
2026-02-14 15:09:46 +0200

    added gradient effect

--- src/components/ui/SectionTitle/SectionTitle.tsx
+++ src/components/ui/SectionTitle/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import GradientText from "../GradientText";
 import style from "./SectionTitle.module.scss";
 
 interface IProps {
@@ -13,8 +14,18 @@ export const SectionTitle: NextPage<IProps> = (props) => {
   return (
     <div className={[style.sectionTitle, className].join(" ")}>
       {!!bgText && <p className={style.sectionTitle__bgText}>{bgText}</p>}
-      <h2 className={style.sectionTitle__title} title={`${title} section`}>
-        {title}
+      <h2
+        className={style.sectionTitle__title}
+        title={`${title} section`}
+        style={{ background: 'none', WebkitTextFillColor: 'unset', WebkitTextStroke: '0' }}
+      >
+        <GradientText
+          colors={["#5227FF", "#7b3b79", "#7765b4"]}
+          animationSpeed={8}
+          showBorder={false}
+        >
+          {title}
+        </GradientText>
       </h2>
     </div>
   );
