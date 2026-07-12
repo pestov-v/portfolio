01bbd005275d534410624f1d80293d70f294cc66
Volodymyr Pestov <pestov.v@yahoo.com>
2026-07-12 19:19:23 +0200

    style(timeline): reduce mobile content padding to 1rem

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -58,7 +58,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
                 </div>
               </div>
 
-              <div className="relative pl-20 pr-4 md:pl-4 w-full">
+              <div className="relative px-4 md:pl-4 w-full">
                 <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                   {item.title}
                 </h3>
